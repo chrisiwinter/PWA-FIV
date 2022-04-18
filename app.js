b2ac9ec9a736da1b97c8c3c7c0209969c82509e3
chrisiwinter <christopher.winter2014@gmail.com>
2022-04-19 01:40:20 +0200

    PWA

---
+++ app.js
@@ -0,0 +1,66 @@
+const app = Vue.createApp({
+    data() {
+        return{
+            Rechner: false,
+            Puntigamer: false,
+            Murauer: false,
+            Wieselburger: false,
+            Zwettler: false,
+            Goesser: false,
+            picture: 'Bier.jpg',
+            AusgabeP: 0,
+            AusgabeM: 0,
+            AusgabeW: 0,
+            AusgabeZ: 0,
+            AusgabeG: 0,
+            Eingabe:  0,
+
+
+        }
+    },
+    methods: {
+        BerechnungPreis(){
+            this.Ausgabe = this.Eingabe
+            this.Rechner = true
+            this.AusgabeP = this.Eingabe  / 23,05
+            this.AusgabeM = this.Eingabe  / 24,49
+            this.AusgabeW = this.Eingabe  / 24,07
+            this.AusgabeZ = this.Eingabe  / 24,00
+            this.AusgabeG = this.Eingabe  / 23,26 
+
+        },
+        ZeigePuntigamer(){
+            if(this.Rechner){
+                this.Puntigamer = !this.Puntigamer
+            }
+
+        },
+        ZeigeMurauer(){
+            if(this.Rechner){
+            this.Murauer = !this.Murauer
+            }
+
+        },
+        ZeigeWieselburger(){
+            if(this.Rechner){
+            this.Wieselburger = !this.Wieselburger
+            }
+
+        },
+        ZeigeZwettler(){
+            if(this.Rechner){
+            this.Zwettler = !this.Zwettler
+            }
+
+        },
+        ZeigeGoesser(){
+            if(this.Rechner){
+            this.Goesser = !this.Goesser
+            }
+
+        }
+
+    }
+})
+
+app.mount('#app')
