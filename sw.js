b4ac88ba87bc009f8813955ba8d1980e340fbdf8
chrisiwinter <christopher.winter2014@gmail.com>
2022-04-19 02:01:41 +0200

    PWA

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
 self.addEventListener('install', function(event) {
     event.waitUntil(
       caches.open('sw-cache').then(function(cache) {
-        return cache.add('index.html');
+        return cache.addAll([
+          './index.html',
+          './style.css',
+          './app.js'
+        ]);
       })
     );
   });
